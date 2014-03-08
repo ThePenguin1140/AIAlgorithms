b0dc18827f706f35064cd0651afbfdf977cef2ec
Jason Würtz <jwuertz@upei.ca>
2014-03-08 12:40:58 -0400

    removed timer that prints out searching method

--- as2/hillclimb/driver.java
+++ as2/hillclimb/driver.java
@@ -1,25 +1,9 @@
 package hillclimb;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class driver {
 	public static void main(String[] args){
-		Timer tm = new Timer();
-		tm.schedule(new TimerTask(){
-			int count = 5;
-			String loading = "Searching";
-			@Override
-			public void run() {
-				loading+=".";
-				count--;
-				if(count==0){
-					loading="Searching";
-					count=5;
-				}
-				System.out.println(loading);
-			}
-		}, 0, 100);
+		System.out.println("Searching...");
 		while(true){
 			EightQueens problem = new EightQueens();
 			EightQueens solution = HillClimb.solve(problem, problem.initialState);
