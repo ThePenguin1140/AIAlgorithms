a37ba8abc1dcf265396f96e264e611e1baa6f89c
Jason Wuertz <jason@wuertz.net>
2014-03-09 16:37:49 +0000

    added analytical output

--- as2/hillclimb/driver.java
+++ as2/hillclimb/driver.java
@@ -4,19 +4,25 @@ package hillclimb;
 public class driver {
 	public static void main(String[] args){
 		System.out.println("Searching...");
-	//	for(int i=0; i<10; i++){
-      while(true){
+		int amount = 1000;
+		int counter =0;
+		for(int i=0; i<amount; i++){
+//      while(true){
 			EightQueens problem = new EightQueens();
         // System.out.println(problem);
 			EightQueens solution = HillClimb.solve(problem, problem.initialState);
-       //  System.out.println(solution);
-    //     System.out.println(solution.value());
+//         System.out.println(solution);
+//         System.out.println(solution.value());
 			if(solution.value()==0){
-				System.out.println(solution);
-            System.out.println(solution.value());
-				System.out.println("--------------------------------------");
-				break;
+//				System.out.println(solution);
+//				System.out.println(solution.value());
+//				System.out.println("--------------------------------------");
+				counter++;
+//				break;
 			}
 		}
+		System.out.println(counter+" out of "+amount+" solved ");
+		double percentage = (double)counter/amount;
+		System.out.print(percentage*100+"%");
 	}
 }
