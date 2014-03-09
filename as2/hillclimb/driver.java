077c011da6da4ae3ba6fbc6f80fb7e8337ad95e0
Jason Wuertz <jason@wuertz.net>
2014-03-09 18:44:01 -0300

    tested simulated annealing

--- as2/hillclimb/driver.java
+++ as2/hillclimb/driver.java
@@ -1,24 +1,26 @@
 package hillclimb;
 
+import problem.Schedule;
 
 public class driver {
 	public static void main(String[] args){
 		System.out.println("Searching...");
-		int amount = 1000;
+		int amount = 2;
 		int counter =0;
 		for(int i=0; i<amount; i++){
-//      while(true){
 			EightQueens problem = new EightQueens();
-        // 	System.out.println(problem);
-			EightQueens solution = RandomResartHillClimbing.solve(problem, problem.initialState);
-//         	System.out.println(solution);
-//         	System.out.println(solution.value());
+			class MySched implements Schedule{
+
+				@Override
+				public int getTemp(int time) {
+					return (int)Math.pow(Math.E, time);
+				}
+				
+			}
+			MySched schedule = new MySched();
+			EightQueens solution = SimulatedAnnealing.solve(problem, problem.initialState, schedule);
 			if(solution.value()==0){
-//				System.out.println(solution);
-//				System.out.println(solution.value());
-//				System.out.println("--------------------------------------");
 				counter++;
-//				break;
 			}
 		}
 		System.out.println(counter+" out of "+amount+" solved ");
