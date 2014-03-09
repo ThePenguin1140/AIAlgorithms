a452da8720a20807cca8b10a5d5868f4e496f252
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-03-08 21:01:07 -0400

    added ability to move sideways

--- as2/hillclimb/HillClimb.java
+++ as2/hillclimb/HillClimb.java
@@ -7,10 +7,16 @@ import problem.State;
 public class HillClimb {
 	public static <T extends State & Problem<? extends T>> T solve(Problem<T> problem, T initialState){
 		Node<T> current = new Node<T>(initialState);
+      int sideways = 100;
 		do{
 			Node<T> neighbor = highestValueSuccessor(current);
 			if(neighbor.value < current.value)
 				return current.state;
+         else if(neightbor.value == current.value && sideways == 0){
+            return current.state;
+         }
+         if(neighbor.value == current.value)
+            sideways--;
 			current = neighbor;
 		}while(true);
 	}
