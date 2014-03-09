61b9278e8adc3715964e90c26055608a72f69a07
Jason Wuertz <jason@wuertz.net>
2014-03-09 18:05:28 -0300

    fixed formatting

--- as2/hillclimb/HillClimb.java
+++ as2/hillclimb/HillClimb.java
@@ -7,21 +7,21 @@ import problem.State;
 public class HillClimb {
 	public static <T extends State & Problem<? extends T>> T solve(Problem<T> problem, T initialState){
 		Node<T> current = new Node<T>(initialState);
-      int sideways = 100;
+		int sideways = 100;
 		do{
 			Node<T> neighbor = highestValueSuccessor(current);
 			if(neighbor.value > current.value)
 				return current.state;
-         else if(neighbor.value == current.value && sideways == 0){
-            return current.state;
-         }
-         if(neighbor.value == current.value)
-            sideways--;
+			else if(neighbor.value == current.value && sideways == 0){
+				return current.state;
+			}
+			if(neighbor.value == current.value)
+				sideways--;
 			current = neighbor;
 		}while(true);
 	}
 
-	private static <T extends State & Problem<? extends T>> Node<T> highestValueSuccessor(Node<T> current) {
+	protected static <T extends State & Problem<? extends T>> Node<T> highestValueSuccessor(Node<T> current) {
 		return current.highestValueSuccessor(current.state);
 	}
 }
