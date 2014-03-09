6d6ffc33e93b895b3a9fcd8a52ddab247ccfc741
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-03-08 21:42:40 -0400

    added some debug print outs

--- as2/hillclimb/driver.java
+++ as2/hillclimb/driver.java
@@ -4,12 +4,16 @@ package hillclimb;
 public class driver {
 	public static void main(String[] args){
 		System.out.println("Searching...");
-		while(true){
+	//	for(int i=0; i<10; i++){
+      while(true){
 			EightQueens problem = new EightQueens();
+        // System.out.println(problem);
 			EightQueens solution = HillClimb.solve(problem, problem.initialState);
-         System.out.println(solution.value());
+       //  System.out.println(solution);
+    //     System.out.println(solution.value());
 			if(solution.value()==0){
 				System.out.println(solution);
+            System.out.println(solution.value());
 				System.out.println("--------------------------------------");
 				break;
 			}
