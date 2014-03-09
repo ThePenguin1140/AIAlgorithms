94ce975d4330835f1cbbcfb6815c12973ffc450c
Jason Wuertz <jason@wuertz.net>
2014-03-09 18:44:48 -0300

    added random successor method

--- reusable/problem/Node.java
+++ reusable/problem/Node.java
@@ -2,6 +2,7 @@ package problem;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Node<T extends State & Problem<? extends T>> {
 	public T state;
@@ -24,6 +25,7 @@ public class Node<T extends State & Problem<? extends T>> {
 	public Node<T> highestValueSuccessor(T cur) {
 		if(Problem.Actions.values().length==0)
 			return new Node<T>(cur.highestValueSuccessor());
+		// CODE BELOW IS NOT TESTED, CANNOT GUARANTEE FUNCTIONALITY
 		else{
 			ArrayList<T> tmp = new ArrayList<T>(); 
 			for(Problem.Actions action: Problem.Actions.values()){
@@ -32,6 +34,17 @@ public class Node<T extends State & Problem<? extends T>> {
 			Collections.sort(tmp);
 			Node<T> bestChoice = new Node<T>(tmp.get(0));
 			return bestChoice;
+		}
+	}
+	
+	public Node<T> randomSuccessor(T cur){
+		if(Problem.Actions.values().length==0)
+			return new Node<T>(cur.randomSuccessor());
+		// CODE BELOW IS NOT TESTED, CANNOT GUARANTEE FUNCTIONALITY
+		else{
+			Random rd = new Random();
+			int i = rd.nextInt(Problem.Actions.values().length);
+			return new Node<T>(cur.Result(Problem.Actions.values()[i]));
 		}
 	}
 }
